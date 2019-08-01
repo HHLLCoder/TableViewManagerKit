197411feb5dbfa39e46edbadc44dcbf6c14b8974
huangxugen <956380213@qq.com>
2019-08-01 19:15:00 +0800

    提交

---
+++ Example/Pods/Target Support Files/TableViewManagerKit/TableViewManagerKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TableViewManagerKit : NSObject
+@end
+@implementation PodsDummy_TableViewManagerKit
+@end
---
+++ TableViewManagerKit/Classes/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  MoyaTest
+//
+//  Created by 黄 on 2019/8/1.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject
+{
+    func Show() {
+        print("hello")
+    }
+}
